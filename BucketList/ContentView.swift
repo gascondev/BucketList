c43d29e47593431f4d54a9ab34a8e047802810f6
Álvaro Gascón <144269155+gascondev@users.noreply.github.com>
2024-06-22 18:07:22 +0200

    End point two and PartTwo

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,106 +5,50 @@
 //  Created by Álvaro Gascón on 20/6/24.
 //
 
-import MapKit
+import LocalAuthentication
 import SwiftUI
 
 
-struct Location: Identifiable {
-    let id = UUID()
-    var name: String
-    var coordinate: CLLocationCoordinate2D
-}
+
 
 
 struct ContentView: View {
     
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
-    
-   @State private var position = MapCameraPosition.region(
-        MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
-            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-        )
-    )
+    @State private var isUnlocked = false
     
     var body: some View {
         VStack {
-            
-            // Mostrar marcadores
-//            Map {
-//                ForEach(locations) { location in
-////                    // Marcadores
-////                    Marker(location.name, coordinate: location.coordinate)
-//                    
-////                    // Marcadores personalizados
-////                    Annotation(location.name, coordinate: location.coordinate) {
-////                        Text(location.name)
-////                            .font(.headline)
-////                            .padding()
-////                            .background(.blue)
-////                            .foregroundStyle(.white)
-////                            .clipShape(.capsule)
-////                    }
-////                    .annotationTitles(.hidden)
-////                }
-//            }
-            
-            // Mostrar ubicación real.
-            MapReader { proxy in
-                Map()
-                    .onTapGesture { position in
-                        if let coordinate = proxy.convert(position, from: .local) {
-                            print(coordinate)
-                        }
-                    }
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
             }
-            
-            
-            
-            
-            
-            // Mostrar posición con actualización instantanea
-//            Map(position: $position)
-//                .onMapCameraChange(frequency: .continuous) { context in
-//                    print(context.region)
-//                }
-            
-//            // Mostrar posición al finalizar movimiento
-//            Map(position: $position)
-//                .onMapCameraChange { context in
-//                    print(context.region)
-//                }
-            
-//            Map(position: $position)
-//                .mapStyle(.hybrid(elevation: .realistic))
-            
-            
-            // Botones de ubicaciones
-//            HStack(spacing: 50) {
-//                Button("Paris") {
-//                    position = MapCameraPosition.region(
-//                        MKCoordinateRegion(
-//                            center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
-//                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-//                        )
-//                    )
-//                }
-//
-//                Button("Tokyo") {
-//                    position = MapCameraPosition.region(
-//                        MKCoordinateRegion(
-//                            center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
-//                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-//                        )
-//                    )
-//                }
-//            }
-            
-            
         }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        // check whether biometric authentication is possible
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                // it's possible, so go ahead and use it
+                let reason = "We need to unlock your data."
+
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                    // authentication has now completed
+                    if success {
+                        
+                        isUnlocked = true
+                        
+                    } else {
+                        // there was a problem
+                    }
+                }
+            } else {
+                // no biometrics
+            }
     }
 }
 
